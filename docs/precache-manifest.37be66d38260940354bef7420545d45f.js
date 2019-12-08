342592f39febbd2e34d982b2fb3339f68c820777
Arthur Agombart <arthur@Lemongrab.local>
2019-12-08 20:08:50 +0100

    push

---
+++ docs/precache-manifest.37be66d38260940354bef7420545d45f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18ea6ca6fe81807a96ae4be6527b064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff64c7130e55e949ce1b",
+    "url": "/static/css/main.3454bd7f.chunk.css"
+  },
+  {
+    "revision": "5bea75a17394ab407f0d",
+    "url": "/static/js/2.e95a3c05.chunk.js"
+  },
+  {
+    "revision": "ff64c7130e55e949ce1b",
+    "url": "/static/js/main.e6903f01.chunk.js"
+  },
+  {
+    "revision": "3a108b3b98962c0cc362",
+    "url": "/static/js/runtime-main.a3fba3e2.js"
+  }
+]);
